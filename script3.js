ff8fee3ca6939b76eb9d569ad70e521715dc677d
Giox367 <111007060+Giox367@users.noreply.github.com>
2022-12-22 16:18:24 +0100

    .

--- script3.js
+++ script3.js
@@ -1,2 +1,21 @@
-// L'oggetto Date
+// L'oggetto Date, le funzioni setinterval e setimeout
 
+const miaData = new Date(); // "Creo una istanza di Date, l'oggetto date inizia a contare dall'1 gennaio 1970"
+
+miaData.getFullYear() // Usiamo i metodi dell'oggetto Date per modificare o estrarre valori da una data
+
+setInterval(()=>{
+
+const container = document.getElementById('container')
+const crdiv = document.createElement('div')
+container.appendChild(crdiv)
+crdiv.innerHTML = 'Ciao'
+},5000)
+
+setTimeout(()=>{
+
+    const container = document.getElementById('container')
+    const crdiv = document.createElement('div')
+    container.appendChild(crdiv)
+    crdiv.innerHTML = 'Ciao'
+    },5000)
